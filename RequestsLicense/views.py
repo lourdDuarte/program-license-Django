8e1afb846eeea958888f1bb77cfff5cb52c42066
lourdDuarte <lourdes123duarte@gmail.com>
2022-09-23 20:13:52 -0300

    commit

--- RequestsLicense/views.py
+++ RequestsLicense/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.decorators import login_required
 from RequestsLicense.forms import RequestLicenseForm
 from RequestsLicense.models import RequestsLicense
 
+import smtplib
+
 # Create your views here.
 @login_required
 def request_license(request):
@@ -20,7 +22,33 @@ def request_license(request):
             new_request.date_to = data['date_to']
             new_request.description = data['description']
 
-            new_request.save()
+            # new_request.save()
+            
+            last_name = data['last_name']
+            first_name = data['first_name']
 
+            # mail notice
+            m = ("La persona: "+ last_name + "," + first_name + " acaba de enviar una nueva solicitud " +
+            "de licencia, ingresar al sistema para confirmarlo. ")
+
+
+            message = str(m)
+            destino = 'lourdes123duarte@gmail.com'
+
+            SUBJECT = "Nueva solicitud de licencia"
+            TEXT = message
+
+            text = "Subject: {}\n\n{}".format(SUBJECT, TEXT)
+
+            smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
+            smtpserver.ehlo()
+            smtpserver.starttls()
+            smtpserver.ehlo()
+            smtpserver.login('mail.diplomatura@gmail.com','piucomltoimebgdw')
+
+                # Enviamos el mensaje
+            smtpserver.sendmail('mail.licenses@gmail.com', destino, text)
+                # Cerramos la conexion
+            smtpserver.close()
         
     return render (request, 'perfil/empleado/request.html')
